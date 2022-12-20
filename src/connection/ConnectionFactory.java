c0e7cfc09101121dfb01490c3d6a94cafdf8e9ff
luizfelipetorres <fellipe116@gmail.com>
2022-12-20 13:30:26 -0300

    feat: implement table creation on start application

--- src/connection/ConnectionFactory.java
+++ src/connection/ConnectionFactory.java
@@ -1,23 +1,31 @@
 package connection;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class ConnectionFactory {
 
 	private static final String DRIVER = "org.postgresql.Driver";
 	private static final String URL = "jdbc:postgresql://localhost:5432/puzzle";
 	private static final String USER = "postgres";
-	private static final String PASS = "root";
+	private static final String PASS = "1234";
 
 	public static Connection getConnection() {
 
 		try {
 			Class.forName(DRIVER);
-			return DriverManager.getConnection(URL, USER, PASS);
+			Connection con = DriverManager.getConnection(URL, USER, PASS);
+			createTables(con);
+			return con;
 
 		} catch (ClassNotFoundException | SQLException ex) {
 			throw new RuntimeException("Erro na conexão!!!", ex);
@@ -59,4 +67,20 @@ public class ConnectionFactory {
 		closeConnection(conn, stmt);
 	}
 
+	public static void createTables(Connection con) {
+		StringBuilder str = new StringBuilder();
+		char i;
+		try {
+			Scanner f = new Scanner(Paths.get("sql//puzzle.sql"));
+			while (f.hasNextLine()) {
+				str.append(f.nextLine());
+			}
+			con.createStatement().execute(str.toString());
+		} catch (SQLException e) {
+			System.out.println("Erro na criação das tabelas!");
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
